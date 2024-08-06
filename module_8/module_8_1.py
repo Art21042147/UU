d40eb812b0df260671315a9b25c85eb53441b1b8
Art21042147 <Art21042147@gmail.com>
2024-08-06 22:18:47 +0300

    no conditions

--- module_8/module_8_1.py
+++ module_8/module_8_1.py
@@ -1,11 +1,6 @@
 def add_everything_up(a, b):
     try:
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return a + b
-        elif isinstance(a, str) and isinstance(b, str):
-            return a + b
-        else:
-            raise TypeError
+        return round((a + b), 3)
     except TypeError:
         return str(a) + str(b)
 
