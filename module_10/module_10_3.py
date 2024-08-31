c84664e14a12992b3ae7b673be946a397c79649c
Art21042147 <Art21042147@gmail.com>
2024-08-31 21:52:12 +0300

    Банковские операции VS Code

--- module_10/module_10_3.py
+++ module_10/module_10_3.py
@@ -7,32 +7,27 @@ class Bank:
     def __init__(self):
         self.balance = 0
         self.lock = Lock()
-        self.print_lock = Lock()
 
     def deposit(self):
         for _ in range(100):
             coming = randint(50, 500)
             self.balance += coming
+            print(f"Пополнение: {coming}. Баланс: {self.balance}")
 
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
-            with self.print_lock:
-                print(f"Пополнение: {coming}. Баланс: {self.balance}")
             sleep(0.001)
 
     def take(self):
         for _ in range(100):
             spending = randint(50, 500)
-            with self.print_lock:
-                print(f"Запрос на {spending}")
+            print(f"Запрос на {spending}")
 
             if spending <= self.balance:
                 self.balance -= spending
-                with self.print_lock:
-                    print(f"Снятие: {spending}. Баланс: {self.balance}")
+                print(f"Снятие: {spending}. Баланс: {self.balance}")
             else:
-                with self.print_lock:
-                    print("Запрос отклонён, недостаточно средств")
+                print("Запрос отклонён, недостаточно средств")
                 self.lock.acquire()
             sleep(0.001)
 
