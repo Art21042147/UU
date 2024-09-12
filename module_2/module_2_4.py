c4054f07b21ccf05031328e6e007321934ad647c
Art21042147 <Art21042147@gmail.com>
2024-09-12 10:42:00 +0300

    renew module_2_4

--- module_2/module_2_4.py
+++ module_2/module_2_4.py
@@ -1,15 +1,14 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
 primes = []
 not_primes = []
-for i in numbers[1:]:
-    is_prime = True
-    for j in range(2, i):
-        if i % j == 0:
-            is_prime = False
+
+for number in numbers[1:]:
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            not_primes.append(number)
             break
-    if is_prime:
-        primes.append(i)
     else:
-        not_primes.append(i)
-print('Primes:', primes)
-print('Not Primes:', not_primes)
+        primes.append(number)
+
+print(f'Primes: {primes}\nNot primes: {not_primes}')
