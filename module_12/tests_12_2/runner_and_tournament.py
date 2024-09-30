5ae015d5bf016199dba1612741094f2d9632db70
Art21042147 <Art21042147@gmail.com>
2024-09-30 20:18:21 +0300

    Merge 12_2

---
+++ module_12/tests_12_2/runner_and_tournament.py
@@ -0,0 +1,49 @@
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        times = {participant.name: self.full_distance / participant.speed
+                 for participant in self.participants}
+
+        time_result = sorted(times.items(), key=lambda item: item[1])
+
+        finishers = {i + 1: participant[0] for i, participant in enumerate(time_result)}
+
+        return finishers
+
+    # def start(self):
+    #     finishers = {}
+    #     place = 1
+    #     while self.participants:
+    #         for participant in self.participants:
+    #             participant.run()
+    #             if participant.distance >= self.full_distance:
+    #                 finishers[place] = participant
+    #                 place += 1
+    #                 self.participants.remove(participant)
+    #
+    #     return finishers
